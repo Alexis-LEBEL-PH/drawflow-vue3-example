b55a84e82466ecd6d2a60421884d66d193bf17c5
Alexis LEBEL <alexis.lebel@poclain.com>
2023-01-25 14:05:40 +0100

    [BLOCKS] MemoryStoreNode

--- src/components/nodes/MemoryStoreNode.vue
+++ src/components/nodes/MemoryStoreNode.vue
@@ -1,33 +1,12 @@
 <template>
-<div ref="el">
-    <nodeHeader  title="Script"/>
-    <p>Open in navbar</p>
-    <el-button type="info"  size="small" @click="drawer = true">Edit</el-button>
-    <teleport to="body">
-        <el-drawer
-        v-model="drawer"
-        title="Edit Optinos"
-        :direction="direction"
-        :before-close="handleClose"
-
-        >
-            <p>Autosave</p>
-            <el-input
-            v-model="textarea"
-            :rows="8"
-            df-script
-             @change="updateSelect" 
-            type="textarea"
-            placeholder="Please input"
-        />
-        </el-drawer>
-    </teleport>
-
-</div>
+    <div ref="el">
+        <nodeHeader title="Store in memory" />
+        <div>Stocke dans la mémoire</div>
+    </div>
 </template>
 
 <script>
-import { defineComponent, ref, getCurrentInstance, nextTick, onMounted } from 'vue'
+import { defineComponent, ref, getCurrentInstance, nextTick, readonly, onMounted } from 'vue'
 import nodeHeader from './nodeHeader.vue'
 import { ElMessageBox } from 'element-plus'
 
@@ -37,47 +16,48 @@ export default defineComponent({
         nodeHeader
     },
     setup() {
+        const dataNode = ref({});
         const el = ref(null);
         const textarea = ref('');
         let df = null
         const nodeId = ref(0);
-        const dataNode = ref({});
         const drawer = ref(false);
         const direction = ref('rtl');
         const handleClose = (done) => {
             ElMessageBox.confirm('Are you sure you want to close this?')
                 .then(() => {
-                done()
+                    done()
                 })
                 .catch(() => {
-                // catch error
+                    // catch error
                 })
         }
-     df = getCurrentInstance().appContext.config.globalProperties.$df.value;
-    
-     const updateSelect = (value) => {
+        df = getCurrentInstance().appContext.config.globalProperties.$df.value;
+
+        const updateSelect = (value) => {
             dataNode.value.data.script = value;
             df.updateNodeDataFromId(nodeId.value, dataNode.value);
         }
 
-     onMounted(async () => {
+        onMounted(async () => {
             await nextTick()
             nodeId.value = el.value.parentElement.parentElement.id.slice(5)
             dataNode.value = df.getNodeFromId(nodeId.value)
             textarea.value = dataNode.value.data.script;
         });
 
-    return {
-      el,
-      drawer,
-      direction,
-      handleClose,
-      textarea,
-      updateSelect
-    }
+        return {
+            el,
+            drawer,
+            direction,
+            handleClose,
+            textarea,
+            updateSelect,
+            dataNode,
+        }
     },
-    
-   
+
+
 })
 </script>
 <style scoped>
