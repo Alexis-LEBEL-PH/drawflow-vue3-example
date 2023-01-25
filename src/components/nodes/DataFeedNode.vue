6052e5bd234d9db812a6eeb3bb347e965c1145c0
Alexis LEBEL <alexis.lebel@poclain.com>
2023-01-25 14:05:13 +0100

    [BLOCKS] DataFeed Block

--- src/components/nodes/DataFeedNode.vue
+++ src/components/nodes/DataFeedNode.vue
@@ -1,19 +1,14 @@
 <template>
     <div ref="el">
-        <nodeHeader  title="Get/Post"/>
+        <nodeHeader title="Data Feed" />
         <el-select v-model="method" placeholder="Select" @change="updateSelect" size="small" df-method>
-        <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-        >
-        </el-option>
-    </el-select>
-    <br><br>
-        <el-input v-model="url" df-url placeholder="Please input" size="small">
-        <template #prepend>https://</template>
-        </el-input>
+            <el-option v-for="item in data_types" :key="item.value" :label="item.label" :value="item.value">
+            </el-option>
+        </el-select>
+        <!-- <br><br> -->
+        <!-- <el-input v-model="url" df-url placeholder="Please input" size="small">
+            <template #prepend>https://</template>
+        </el-input> -->
     </div>
 </template>
 
@@ -32,39 +27,39 @@ export default defineComponent({
         const url = ref('');
         const method = ref('get');
         const dataNode = ref({});
-        const options = readonly([
+        const data_types = readonly([
             {
-                value: 'get',
-                label: 'GET'
+                value: 'can',
+                label: 'CAN Signal'
             },
             {
-                value: 'post',
-                label: 'POST'
+                value: 'native',
+                label: 'Native Signal'
             }
         ]);
-        
+
         df = getCurrentInstance().appContext.config.globalProperties.$df.value;
 
         const updateSelect = (value) => {
             dataNode.value.data.method = value;
             df.updateNodeDataFromId(nodeId.value, dataNode.value);
         }
-        
-        
+
+
         onMounted(async () => {
             await nextTick()
             nodeId.value = el.value.parentElement.parentElement.id.slice(5)
             dataNode.value = df.getNodeFromId(nodeId.value)
-            
+
             url.value = dataNode.value.data.url;
             method.value = dataNode.value.data.method;
         });
-        
+
         return {
-            el, url, method, options, updateSelect
+            el, url, method, data_types, updateSelect
         }
 
-    }    
-    
+    }
+
 })
 </script>
