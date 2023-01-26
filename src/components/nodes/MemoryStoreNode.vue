6ed1c8de482ad5066f4596d723b8166adbf73091
Alexis LEBEL <alexis.lebel@poclain.com>
2023-01-26 13:11:55 +0100

    [MEMORY] Splitted store/fetch

--- src/components/nodes/MemoryStoreNode.vue
+++ src/components/nodes/MemoryStoreNode.vue
@@ -1,68 +1,60 @@
 <template>
     <div ref="el">
-        <nodeHeader title="Store in memory" />
-        <div>Stocke dans la mémoire</div>
+        <nodeHeader title="Store in Memory" />
+        <p>Variable Number</p>
+        <el-select v-model="method" placeholder="Select" @change="updateSelect" size="small" df-method>
+            <el-option v-for="item in variables" :key="item" :label="item" :value="item">
+            </el-option>
+        </el-select>
+        <!-- <br><br> -->
+        <!-- <el-input v-model="url" df-url placeholder="Please input" size="small">
+            <template #prepend>https://</template>
+        </el-input> -->
     </div>
 </template>
 
 <script>
-import { defineComponent, ref, getCurrentInstance, nextTick, readonly, onMounted } from 'vue'
+import { defineComponent, onMounted, getCurrentInstance, readonly, ref, nextTick } from 'vue'
 import nodeHeader from './nodeHeader.vue'
-import { ElMessageBox } from 'element-plus'
-
 
 export default defineComponent({
     components: {
         nodeHeader
     },
     setup() {
-        const dataNode = ref({});
         const el = ref(null);
-        const textarea = ref('');
-        let df = null
         const nodeId = ref(0);
-        const drawer = ref(false);
-        const direction = ref('rtl');
-        const handleClose = (done) => {
-            ElMessageBox.confirm('Are you sure you want to close this?')
-                .then(() => {
-                    done()
-                })
-                .catch(() => {
-                    // catch error
-                })
-        }
+        let df = null
+        const url = ref('');
+        const selected_variable = ref(0);
+        const dataNode = ref({});
+        const variables = readonly(
+            // A range from 0 to 15 of numbers only
+            Array.from(Array(16).keys())
+        );
+
         df = getCurrentInstance().appContext.config.globalProperties.$df.value;
 
         const updateSelect = (value) => {
-            dataNode.value.data.script = value;
+            dataNode.value.data.method = value;
             df.updateNodeDataFromId(nodeId.value, dataNode.value);
         }
+
 
         onMounted(async () => {
             await nextTick()
             nodeId.value = el.value.parentElement.parentElement.id.slice(5)
             dataNode.value = df.getNodeFromId(nodeId.value)
-            textarea.value = dataNode.value.data.script;
+
+            url.value = dataNode.value.data.url;
+            // selected_variable = dataNode.value.data.method;
         });
 
         return {
-            el,
-            drawer,
-            direction,
-            handleClose,
-            textarea,
-            updateSelect,
-            dataNode,
+            el, url, method: selected_variable, variables, updateSelect
         }
-    },
 
+    }
 
 })
 </script>
-<style scoped>
-p {
-    margin: 5px;
-    margin-bottom: 10px;
-}
-</style>
