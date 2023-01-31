c7881f2e2894e77005e506b7414a4cab6880c57b
Alexis LEBEL <alexis.lebel@poclain.com>
2023-01-31 10:32:14 +0100

    [MEMORY] Can now name memory variables

--- src/components/nodes/MemoryStoreNode.vue
+++ src/components/nodes/MemoryStoreNode.vue
@@ -1,11 +1,9 @@
 <template>
-    <div ref="el">
+    <div ref="el" v-on:focusout="focusOut">
         <nodeHeader title="Store in Memory" />
-        <p>Variable Number</p>
-        <el-select v-model="method" placeholder="Select" @change="updateSelect" size="small" df-method>
-            <el-option v-for="item in variables" :key="item" :label="item" :value="item">
-            </el-option>
-        </el-select>
+        <p>Espace Mémoire</p>
+        <el-select-v2 v-model="memory_space" :options="memory_spaces" clearable allow-create filterable>
+        </el-select-v2>
         <!-- <br><br> -->
         <!-- <el-input v-model="url" df-url placeholder="Please input" size="small">
             <template #prepend>https://</template>
@@ -21,6 +19,21 @@ export default defineComponent({
     components: {
         nodeHeader
     },
+    methods: {
+        focusOut() {
+            if (this.memory_space == "" || this.memory_space == null)
+                return;
+            // If the user has entered a new memory space, add it to the list
+            for (var i = 0; i < this.memory_spaces.length; i++) {
+                if (this.memory_spaces[i].value == this.memory_space) {
+                    return;
+                }
+            }
+            this.memory_spaces.push({ value: this.memory_space, label: this.memory_space });
+            sessionStorage.memory_spaces = JSON.stringify(this.memory_spaces);
+            console.log(sessionStorage.getItem('memory_spaces'));
+        },
+    },
     setup() {
         const el = ref(null);
         const nodeId = ref(0);
@@ -28,10 +41,11 @@ export default defineComponent({
         const url = ref('');
         const selected_variable = ref(0);
         const dataNode = ref({});
-        const variables = readonly(
-            // A range from 0 to 15 of numbers only
-            Array.from(Array(16).keys())
-        );
+        var memory_spaces = [{
+            value: "1",
+            label: "1"
+        }];
+        const memory_space = ref("");
 
         df = getCurrentInstance().appContext.config.globalProperties.$df.value;
 
@@ -40,18 +54,16 @@ export default defineComponent({
             df.updateNodeDataFromId(nodeId.value, dataNode.value);
         }
 
-
         onMounted(async () => {
-            await nextTick()
-            nodeId.value = el.value.parentElement.parentElement.id.slice(5)
-            dataNode.value = df.getNodeFromId(nodeId.value)
-
-            url.value = dataNode.value.data.url;
-            // selected_variable = dataNode.value.data.method;
+            await nextTick();
+            // getting memory spaces from sessionStorage
+            if (sessionStorage.getItem('memory_spaces')) {
+                memory_spaces = JSON.parse(sessionStorage.getItem('memory_spaces'));
+            }
         });
 
         return {
-            el, url, method: selected_variable, variables, updateSelect
+            el, url, method: selected_variable, memory_spaces, memory_space, updateSelect
         }
 
     }
