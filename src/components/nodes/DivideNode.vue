11c9009779ce7ad4b4ddf841623f26a86ff64019
Alexis LEBEL <alexis.lebel@poclain.com>
2023-01-31 11:45:20 +0100

    [OPERATIONS] Reworked division node

--- src/components/nodes/DivideNode.vue
+++ src/components/nodes/DivideNode.vue
@@ -1,9 +1,13 @@
+/*
+ *   Copyright (c) 2023 Alexis LEBEL @ Poclain Hydraulics Industrie
+ *   All rights reserved.
+ */
 <template>
     <div ref="el" v-on:focusin="focusIn">
         <nodeHeader title="Division" />
         <!--- division ÷ icon centered in the container with svg path -->
         <div class="icon_container">
-            <svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="3em" height="3em"
+            <svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="3em" height="3em">
                 style="display: block;">
                 <path
                     d="M512 0C229.248 0 0 229.248 0 512s229.248 512 512 512 512-229.248 512-512S794.752 0 512 0zM512 960C264.576 960 64 759.424 64 512S264.576 64 512 64s448 200.576 448 448-200.576 448-448 448z"
@@ -16,7 +20,7 @@
                 <circle cx="512" cy="740" r="32" fill="#ffffff" p-id="1003"></circle>
             </svg>
         </div>
-        <p>Espace Mémoire</p>
+        <p>Memory Slot</p>
         <el-select-v2 ref="elSelectV2" v-model="memory_space" :options="memory_spaces" clearable>
         </el-select-v2>
     </div>
@@ -32,6 +36,7 @@ export default defineComponent({
     },
     watch: {
         memory_spaces: function () {
+            console.log("memory_spaces changed");
             this.$refs.elSelectV2.refresh();
         }
     },
@@ -39,7 +44,6 @@ export default defineComponent({
         const el = ref(null);
         const nodeId = ref(0);
         // let df = null
-        const url = ref('');
         const dataNode = ref({});
         var memory_spaces = [{
             value: "1",
@@ -54,21 +58,11 @@ export default defineComponent({
         });
 
         return {
-            el, url, memory_spaces, memory_space
+            el, memory_spaces, memory_space
         }
 
     },
     methods: {
-        // focusOut() {
-        // if (this.memory_space == "" || this.memory_space == null)
-        //     return;
-        // // If the user has entered a new memory space, add it to the list
-        // for (var i = 0; i < this.memory_spaces.length; i++) {
-        //     if (this.memory_spaces[i].value == this.memory_space) {
-        //         return;
-        //     }
-        // }
-        // },
         focusIn() {
             this.memory_spaces = JSON.parse(sessionStorage.getItem("memory_spaces"));
         },
@@ -83,6 +77,6 @@ export default defineComponent({
     justify-content: center;
     align-items: center;
     height: 4em;
-    margin: auto 1em;
+    margin-top: -0.5em;
 }
 </style>
