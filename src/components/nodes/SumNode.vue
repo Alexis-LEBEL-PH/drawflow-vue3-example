1122f6a7a34835b1b8c33b8ae1a845e6671f2c96
Alexis LEBEL <alexis.lebel@poclain.com>
2023-01-31 11:45:52 +0100

    [OPERATIONS] Reworked sum node

--- src/components/nodes/SumNode.vue
+++ src/components/nodes/SumNode.vue
@@ -2,11 +2,22 @@
     <div ref="el">
         <nodeHeader title="Sum" />
         <!--- + icon centered in the container with svg path -->
-        <svg class="icon" viewBox="-2350 255 4096 4096" xmlns="http://www.w3.org/2000/svg" width="150" height="150">
-            <path d="M512 0C229.248 0 0 229.248 0 512s229.248 512 512 512 512-229.248 512-512S794.752 0 512 0zM512 960C264.576 960 64 759.424 64 512S264.576 64 512 64s448 200.576 448 448-200.576 448-448 448z" fill="#ffffff" p-id="1000"></path>
-            <path d="M512 320c-17.664 0-32 14.336-32 32v320c0 17.664 14.336 32 32 32s32-14.336 32-32V352c0-17.664-14.336-32-32-32z" fill="#ffffff" p-id="1001"></path>
-            <path d="M352 480c-17.664 0-32 14.336-32 32s14.336 32 32 32h320c17.664 0 32-14.336 32-32s-14.336-32-32-32H352z" fill="#ffffff" p-id="1002"></path>
-        </svg>
+        <div class="icon_container">
+            <svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="3em" height="3em">
+                <path
+                    d="M512 0C229.248 0 0 229.248 0 512s229.248 512 512 512 512-229.248 512-512S794.752 0 512 0zM512 960C264.576 960 64 759.424 64 512S264.576 64 512 64s448 200.576 448 448-200.576 448-448 448z"
+                    fill="#ffffff" p-id="1000"></path>
+                <path
+                    d="M512 320c-17.664 0-32 14.336-32 32v320c0 17.664 14.336 32 32 32s32-14.336 32-32V352c0-17.664-14.336-32-32-32z"
+                    fill="#ffffff" p-id="1001"></path>
+                <path
+                    d="M352 480c-17.664 0-32 14.336-32 32s14.336 32 32 32h320c17.664 0 32-14.336 32-32s-14.336-32-32-32H352z"
+                    fill="#ffffff" p-id="1002"></path>
+            </svg>
+        </div>
+        <p>Memory Slot</p>
+        <el-select-v2 ref="elSelectV2" v-model="memory_space" :options="memory_spaces" clearable>
+        </el-select-v2>
     </div>
 </template>
 
@@ -21,32 +32,40 @@ export default defineComponent({
     setup() {
         const el = ref(null);
         const nodeId = ref(0);
-        let df = null
-        const url = ref('');
+        // let df = null
         const dataNode = ref({});
-
-        df = getCurrentInstance().appContext.config.globalProperties.$df.value;
-
-        const updateSelect = (value) => {
-            dataNode.value.data.method = value;
-            df.updateNodeDataFromId(nodeId.value, dataNode.value);
-        }
-
+        var memory_spaces = [{
+            value: "1",
+            label: "1"
+        }];
+        const memory_space = ref("1");
 
         onMounted(async () => {
-            await nextTick()
-            nodeId.value = el.value.parentElement.parentElement.id.slice(5)
-            dataNode.value = df.getNodeFromId(nodeId.value)
-
-            url.value = dataNode.value.data.url;
-            // method.value = dataNode.value.data.method;
+            await nextTick();
+            if (sessionStorage.getItem("memory_spaces"))
+                memory_spaces = JSON.parse(sessionStorage.getItem("memory_spaces"));
         });
 
         return {
-            el, url,  updateSelect
+            el, memory_spaces, memory_space
         }
 
-    }
+    },
+    methods: {
+        focusIn() {
+            this.memory_spaces = JSON.parse(sessionStorage.getItem("memory_spaces"));
+        },
+    },
 
 })
 </script>
+
+<style scoped>
+.icon_container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 4em;
+    margin-top: -0.5em;
+}
+</style>
