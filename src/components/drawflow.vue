c808f805e935fcdb4143f62319a60caa4bc75b2b
Alexis LEBEL <alexis.lebel@poclain.com>
2023-02-03 10:04:41 +0100

    [NODES] Added can message Node

--- src/components/drawflow.vue
+++ src/components/drawflow.vue
@@ -54,6 +54,7 @@ import Node7 from './nodes/DivideNode.vue'
 import Node8 from './nodes/ProductNode.vue'
 import Node9 from './nodes/InsideRangeNode.vue'
 import Node10 from './nodes/TimeNode.vue'
+import Node11 from './nodes/CanMessageNode.vue'
 import axios from 'axios'
 
 
@@ -62,16 +63,16 @@ export default {
   name: 'drawflow',
   setup() {
     const listNodes = readonly([
-      // {
-      //   name: 'Data Feed',
-      //   block_type: 0,
-      //   color: '#49494970',
-      //   item: 'Node1',
-      //   input: 0,
-      //   output: 1
-      // },
       {
-        name: 'Send MQTT',
+        name: 'Can Message',
+        block_type: 0,
+        color: '#49494970',
+        item: 'Node11',
+        input: 0,
+        output: 1
+      },
+      {
+        name: 'Send to Cloud',
         block_type: 1,
         color: 'blue',
         item: 'Node2',
@@ -217,16 +218,15 @@ export default {
       editor.value = new Drawflow(id, Vue, internalInstance.appContext.app._context);
       editor.value.start();
 
-      // editor.value.registerNode('Node1', Node1, {}, {});
       editor.value.registerNode('Node2', Node2, {}, {});
       editor.value.registerNode('Node3', Node3, {}, {});
-      // editor.value.registerNode('Node4', Node4, {}, {});
       editor.value.registerNode('Node5', Node5, {}, {});
       editor.value.registerNode('Node6', Node6, {}, {});
       editor.value.registerNode('Node7', Node7, {}, {});
       editor.value.registerNode('Node8', Node8, {}, {});
       editor.value.registerNode('Node9', Node9, {}, {});
       editor.value.registerNode('Node10', Node10, {}, {});
+      editor.value.registerNode('Node11', Node11, {}, {});
 
       editor.value.import({ "drawflow": { "Home": { "data": { "4": { "id": 4, "name": "Node8", "data": {}, "class": "Node8", "html": "Node8", "typenode": "vue", "inputs": { "input_1": { "connections": [] } }, "outputs": { "output_1": { "connections": [{ "node": "5", "output": "input_2" }] } }, "pos_x": 611.5714285714286, "pos_y": 268 }, "5": { "id": 5, "name": "Node7", "data": {}, "class": "Node7", "html": "Node7", "typenode": "vue", "inputs": { "input_1": { "connections": [{ "node": "4", "input": "output_1" }] } }, "outputs": { "output_1": { "connections": [{ "node": "6", "output": "input_1" }] } }, "pos_x": 1045.4285714285713, "pos_y": 208 }, "6": { "id": 6, "name": "Node3", "data": {}, "class": "Node3", "html": "Node3", "typenode": "vue", "inputs": { "input_1": { "connections": [{ "node": "5", "input": "output_1" }] } }, "outputs": {}, "pos_x": 1526, "pos_y": 206.42857142857142 } } } } });
 
---
+++ src/components/nodes/CanMessageNode.vue
@@ -0,0 +1,93 @@
+/*
+ *   Copyright (c) 2023 Alexis LEBEL @ Poclain Hydraulics Industrie
+ *   All rights reserved.
+ */
+<template>
+    <div ref="el">
+        <nodeHeader title="Can Message" />
+        <div class="root">
+            <label>Can channel :</label>
+            <el-select-v2 v-model="channel" :options="CAN_Channels" placeholder="Select" @change="updateSelect"
+                size="small" df-method>
+            </el-select-v2>
+            <!-- <br><br> -->
+            <label>Can ID :</label>
+            <el-input v-model="can_id" df- placeholder="Please input" @change="updateValues" size="small">
+                <template #prepend>#</template>
+            </el-input>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, getCurrentInstance, readonly, ref, nextTick } from 'vue'
+import nodeHeader from './nodeHeader.vue'
+
+export default defineComponent({
+    components: {
+        nodeHeader
+    },
+    setup() {
+        const el = ref(null);
+        const nodeId = ref(0);
+        let df = null;
+        const can_id = ref('');
+        const channel = ref('1');
+        const dataNode = ref({});
+        const CAN_Channels = readonly([
+            {
+                value: '1',
+                label: '1'
+            },
+            {
+                value: '2',
+                label: '2'
+            }
+        ]);
+
+        df = getCurrentInstance().appContext.config.globalProperties.$df.value;
+
+        const updateValues = () => {
+            const reg = /^[0-9a-fA-F]{3,8}$/;
+            if(!can_id.value.match(reg)) {
+                can_id.value = '';
+            }
+            dataNode.value.data.channel = channel;
+            dataNode.value.data.can_id = can_id;
+            df.updateNodeDataFromId(nodeId.value, dataNode.value);
+        }
+
+        // const hex_format = (value) => {
+        //     // This is the formatter that only allows hex data to be entered in the input
+        //     console.log(value);
+        //     const reg = /^[0-9a-fA-F]*$/;
+        //     if (reg.test(value)) {
+        //         return value;
+        //     } else {
+        //         return value.slice(0, -1);
+        //     }
+        // }
+
+
+        onMounted(async () => {
+            await nextTick()
+            nodeId.value = el.value.parentElement.parentElement.id.slice(5)
+            dataNode.value = df.getNodeFromId(nodeId.value)
+        });
+
+        return {
+            el, channel, CAN_Channels, updateValues, can_id
+        }
+
+    }
+
+})
+</script>
+
+<style scoped>
+.root {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+}
+</style>
