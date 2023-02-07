6980543ad5a615f19633a88658fa8fa03d7840d4
Alexis LEBEL <alexis.lebel@poclain.com>
2023-02-07 08:54:46 +0100

    [NODES/CLOUD] Cloud node less advanced

--- src/components/nodes/SendCloudNode.vue
+++ src/components/nodes/SendCloudNode.vue
@@ -5,22 +5,31 @@
 <template>
     <div ref="el">
         <nodeHeader title="Send MQTT" />
-        <label>Encoding type :
+        <!-- cloud in svg path -->
+        <div class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="5em" height="5em" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="feather feather-cloud">
+                <path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z"></path>
+            </svg>
+        </div>
+
+        <!-- <label>Encoding type :
             <select v-model="selected_encoding_type" placeholder="Select" size="small" df-method @change="selectChange">
                 <option v-for="(item, index) in encoding_types" :key="index" :value="item">
                     {{ item }}
                 </option>
             </select>
-        </label>
+        </label> -->
         <tr></tr>
         <br />
-        <label>
+        <!-- <label>
             Offset / Scale : <br>
             <ElInput v-model="offset" type="number" min=0 max=255 size="small" @change="selectChange"/>
             <ElInput v-model="scale" type="number" min=0 max=65535 size="small" @change="selectChange"/>
             Time resolution (us) : <br>
             <ElInput v-model="time_resolution" type="number" min=0 max=4000000000 size="small" @change="selectChange"/>
-        </label>
+        </label> -->
     </div>
 </template>
 
@@ -54,7 +63,7 @@ export default defineComponent({
         });
         const offset = ref(0);
         const time_resolution = ref(0);
-        const scale  = ref(0);
+        const scale = ref(0);
         const MQTTValueID = ref(0);
         const selected_encoding_type = ref(0);
         const drawer = ref(false);
@@ -77,7 +86,7 @@ export default defineComponent({
         }
     },
     methods: {
-        selectChange(){
+        selectChange() {
             // This stores the memory space in the node data for export
             this.dataNode.data.offset = this.offset;
             this.dataNode.data.scale = this.scale;
@@ -95,5 +104,31 @@ export default defineComponent({
 p {
     margin: 5px;
     margin-bottom: 10px;
+}
+
+.icon {
+    height: 5em;
+    width: 5em;
+    /* display: block; */
+    margin: auto;
+    justify-content: center;
+    align-items: center;
+}
+
+svg {
+    height: 5em;
+    width: 5em;
+    /* fill: #fff; */
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+    stroke: #fff;
+}
+
+path {
+    height: 5em;
+    width: 5em;
+    margin: auto;
+    /* scale: 2; */
 }
 </style>
